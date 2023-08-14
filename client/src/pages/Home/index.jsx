ad1acf5250ddce076daac6fa58dec797f78b5d69
tinproht123 <tinproht123@gmail.com>
2023-08-14 17:31:11 +0700

    update client

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,42 @@
 import { Box, Typography } from "@mui/material";
+import StyledButton from "../../components/StyledButton";
 
 const HomePage = () => {
   return (
-    <Box>
-      <Typography>Home Page</Typography>
+    <Box sx={{ width: "100%", maxWidth: "1440px", marginInline: "auto" }}>
+      <Box position="relative">
+        <img src="/images/bg.webp" alt="image-banner" />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: 300,
+            right: 50,
+            textAlign: "right",
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "flex-end",
+          }}
+        >
+          <Typography
+            sx={{ fontSize: "80px", fontWeight: 700, color: "#fff" }}
+            mb={4}
+            component="h1"
+          >
+            REACH YOUR BEST
+          </Typography>
+          <Typography
+            component="span"
+            color="#fff"
+            width={400}
+            display="inline-block"
+            mb={4}
+          >
+            Whether you’re training for a marathon or your biggest season yet,
+            we’re here to help you make serious progress.
+          </Typography>
+          <StyledButton type="light">SIGN UP</StyledButton>
+        </Box>
+      </Box>
     </Box>
   );
 };
