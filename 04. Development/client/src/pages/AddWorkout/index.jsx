2bff64825ed0a4913bf7ebc1b47961305418d57c
Minh Anh <tinproht123@gmail.com>
2023-08-24 23:12:39 +0700

    update code

--- 04. Development/client/src/pages/AddWorkout/index.jsx
+++ 04. Development/client/src/pages/AddWorkout/index.jsx
@@ -1,32 +1,124 @@
-import { Box, TextField } from "@mui/material";
+import {
+  Box,
+  TextField,
+  MenuItem,
+  Button,
+  TextareaAutosize,
+} from "@mui/material";
 import { DatePicker, TimePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 import { useFormik } from "formik";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
+import { getActivities } from "../../store/features/adminSlice";
 
 const AddWorkout = () => {
+  const dispatch = useDispatch();
+  const { activities } = useSelector((state) => state.admin);
+
+  const [chosenActivity, setChosenActivity] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [pace, setPace] = useState(null);
+  const [caloriesBurned, setCaloriesBurned] = useState(null);
+
+  useEffect(() => {
+    dispatch(getActivities());
+  }, [dispatch]);
+
   const validationSchema = yup.object({
     name: yup.string("Enter name of workout"),
     hourStart: yup.string().required("Time is required"),
     hourEnd: yup.string().required("Time is required"),
     dateStart: yup.date().required("Date is required"),
     dateEnd: yup.string().required("Date is required"),
+    activity: yup
+      .string()
+      .oneOf(activities.map((activity) => activity.name))
+      .required("Select your workout&apos;s activity"),
+    distance: yup.number().required("Distance is required"),
+    pace: yup.number(),
+    caloriesBurned: yup.number(),
+    note: yup.string(),
   });
 
   const formik = useFormik({
     initialValues: {
       name: "",
-      hourState: "",
-      hourEnd: "",
+      hourStart: dayjs(),
+      hourEnd: dayjs(),
       dateStart: dayjs(),
       dateEnd: dayjs(),
+      activity: "",
+      distance: null,
+      pace,
+      caloriesBurned,
+      note: "",
     },
     validationSchema,
     onSubmit: (val) => {
-      console.log(val);
+      const workoutData = {
+        
+      }
     },
   });
+
+  const calculateDuration = () => {
+    if (
+      formik.values.hourEnd.length !== 0 &&
+      formik.values.hourStart.length !== 0 &&
+      formik.values.dateEnd.length !== 0 &&
+      formik.values.dateStart.length !== 0
+    ) {
+      const timeEnd = dayjs(
+        formik.values.dateEnd + formik.values.hourEnd
+      ).format("YYYY-MM-DD HH:mm:ss");
+      const timeStart = dayjs(
+        formik.values.dateStart + formik.values.hourStart
+      ).format("YYYY-MM-DD HH:mm:ss");
+
+      const seconds = dayjs(timeEnd).diff(dayjs(timeStart), "seconds");
+
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const remainingSeconds = seconds % 60;
+
+      setDuration(`${hours}:${minutes}:${remainingSeconds}`);
+    }
+  };
+
+  const calculatePace = () => {
+    if (formik.values.distance !== null && duration !== null) {
+      const timeString = duration;
+      const [hours, minutes, seconds] = timeString.split(":");
+
+      const totalSeconds =
+        parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
+      const res = (formik.values.distance * 1000) / totalSeconds;
+
+      setPace(res.toFixed(2));
+    }
+  };
+
+  const calculateCaloriesBurned = () => {
+    let res;
+    if (chosenActivity.type === "no-gym") {
+      res = chosenActivity.met * pace * 100;
+    } else {
+      const timeString = duration;
+      const arr = timeString.split(":");
+
+      res = chosenActivity.met * parseInt(arr[0]) * 100;
+    }
+    setCaloriesBurned(res.toFixed(2));
+  };
+
+  useEffect(() => {
+    setChosenActivity(
+      activities.find((item) => item.name === formik.values.activity)
+    );
+  }, [formik.values.activity, activities]);
 
   return (
     <Box
@@ -42,6 +134,7 @@ const AddWorkout = () => {
     >
       <form onSubmit={formik.handleSubmit}>
         <TextField
+          size="small"
           fullWidth
           id="name"
           label="Workout name"
@@ -52,7 +145,7 @@ const AddWorkout = () => {
           helperText={formik.touched.name && formik.errors.name}
           sx={{ marginBottom: "20px" }}
         />
-        <Box display="flex">
+        <Box display="flex" sx={{ marginBottom: "20px" }}>
           <TimePicker
             views={["hours", "minutes", "seconds"]}
             sx={{ flex: 1, mr: 1 }}
@@ -64,6 +157,8 @@ const AddWorkout = () => {
             slotProps={{
               textField: {
                 variant: "outlined",
+                size: "small",
+                required: true,
                 error:
                   formik.touched.hourStart && Boolean(formik.errors.hourStart),
                 helperText: formik.touched.hourStart && formik.errors.hourStart,
@@ -82,6 +177,8 @@ const AddWorkout = () => {
             slotProps={{
               textField: {
                 variant: "outlined",
+                size: "small",
+                required: true,
                 error:
                   formik.touched.dateStart && Boolean(formik.errors.dateStart),
                 helperText: formik.touched.dateStart && formik.errors.dateStart,
@@ -89,6 +186,157 @@ const AddWorkout = () => {
             }}
           />
         </Box>
+        <TextField
+          size="small"
+          fullWidth
+          required
+          select
+          id="activity"
+          label="Activity"
+          value={formik.values.activity}
+          onChange={formik.handleChange("activity")}
+          error={formik.touched.activity && Boolean(formik.errors.activity)}
+          helperText={formik.touched.activity && formik.errors.activity}
+          sx={{ marginBottom: "20px" }}
+        >
+          {activities.map((activity) => (
+            <MenuItem key={activity.id} value={activity.name}>
+              {activity.name}
+            </MenuItem>
+          ))}
+        </TextField>
+        {chosenActivity !== undefined && (
+          <>
+            <Box display="flex" sx={{ marginBottom: "20px" }}>
+              <TimePicker
+                views={["hours", "minutes", "seconds"]}
+                sx={{ flex: 1, mr: 1 }}
+                label="Time end"
+                value={formik.values.hourEnd}
+                onChange={(time) => {
+                  formik.setFieldValue("hourEnd", time);
+                }}
+                slotProps={{
+                  textField: {
+                    variant: "outlined",
+                    size: "small",
+                    required: true,
+                    error:
+                      formik.touched.hourEnd && Boolean(formik.errors.hourEnd),
+                    helperText: formik.touched.hourEnd && formik.errors.hourEnd,
+                  },
+                }}
+              />
+              <DatePicker
+                disablePast
+                label="Date start"
+                format="YYYY/MM/DD"
+                value={formik.values.dateEnd}
+                onChange={(date) => {
+                  formik.setFieldValue("dateEnd", date);
+                }}
+                sx={{ flex: 1, ml: 1 }}
+                slotProps={{
+                  textField: {
+                    variant: "outlined",
+                    size: "small",
+                    required: true,
+                    error:
+                      formik.touched.dateEnd && Boolean(formik.errors.dateEnd),
+                    helperText: formik.touched.dateEnd && formik.errors.dateEnd,
+                  },
+                }}
+              />
+            </Box>
+            <Box display="flex" gap="20px" mb={"20px"}>
+              <TextField
+                label="Duration"
+                value={duration}
+                readOnly
+                size="small"
+                InputProps={{ readOnly: true }}
+                InputLabelProps={{ shrink: true }}
+              />
+              <Button
+                variant="outlined"
+                sx={{ fontWeight: 700, flexShrink: 0 }}
+                onClick={calculateDuration}
+              >
+                Calculate
+              </Button>
+            </Box>
+            {chosenActivity?.type === "no-gym" && (
+              <>
+                <TextField
+                  required
+                  size="small"
+                  id="distance"
+                  label="Distance (km)"
+                  value={formik.values.distance}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={
+                    formik.touched.distance && Boolean(formik.errors.distance)
+                  }
+                  helperText={formik.touched.distance && formik.errors.distance}
+                  sx={{ marginBottom: "20px" }}
+                  type="number"
+                />
+
+                <Box display="flex" gap="20px" mb={"20px"}>
+                  <TextField
+                    label="Pace m/s"
+                    value={pace}
+                    placeholder="m/s"
+                    InputProps={{ readOnly: true }}
+                    InputLabelProps={{ shrink: true }}
+                    size="small"
+                  />
+                  <Button
+                    variant="outlined"
+                    sx={{ fontWeight: 700, flexShrink: 0 }}
+                    onClick={calculatePace}
+                  >
+                    Calculate
+                  </Button>
+                </Box>
+              </>
+            )}
+            <Box display="flex" gap="20px" mb={"20px"}>
+              <TextField
+                label="Calories Burned (cal)"
+                value={caloriesBurned}
+                placeholder="m/s"
+                InputProps={{ readOnly: true }}
+                InputLabelProps={{ shrink: true }}
+                size="small"
+              />
+              <Button
+                variant="outlined"
+                sx={{ fontWeight: 700, flexShrink: 0 }}
+                onClick={calculateCaloriesBurned}
+              >
+                Calculate
+              </Button>
+            </Box>
+            <textarea
+              id="note"
+              style={{
+                width: "100%",
+                padding: "10px",
+                fontSize: 16,
+                marginBottom: "30px",
+              }}
+              rows={5}
+              placeholder="Leave a note"
+              value={formik.values.note}
+              onChange={formik.handleChange}
+            ></textarea>
+            <Button type="submit" variant="contained">
+              Save
+            </Button>
+          </>
+        )}
       </form>
     </Box>
   );
